beb8f8bb81f04f912bf68b270b4339511c49857c
Anubhav Ranjan <ranjananubhav7@gmail.com>
2022-04-19 19:56:46 +0530

    added voice recognition

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import axios from "axios";
 import Item from "./items.jsx"
 
+
 function App(){
 
   const [bookName, setbookName] = useState("");
@@ -10,11 +11,17 @@ function App(){
 
   function handleChange(event){
     setbookName(event.target.value);
+    console.log(bookName);
+    axios.get("https://www.googleapis.com/books/v1/volumes?q="+bookName+"&key="+apikey+"&maxResults=40").then(data =>
+    {
+      setresults(data.data.items);
+    });
   }
 
   function handleSubmit(event){
     event.preventDefault();
-    axios.get("https://www.googleapis.com/books/v1/volumes?q="+bookName+"&key="+apikey+"&maxResults=40").then(data =>
+    var bookName1 = document.getElementById("formInput").value;
+    axios.get("https://www.googleapis.com/books/v1/volumes?q="+bookName1+"&key="+apikey+"&maxResults=40").then(data =>
     {
       setresults(data.data.items);
     });
@@ -23,7 +30,12 @@ function App(){
   return (<div className="forM">
   <h1 className="formText">Search for books!</h1>
   <form onSubmit={handleSubmit}>
-    <input type="text" onChange={handleChange} className="form-control formInput" placeholder="type here" autoComplete="off" />
+    <input id="formInput" type="text" onChange={handleChange} className="form-control formInput" placeholder="type here" autoComplete="off" />
+    
+    <div>
+      <i id="start_button" onClick={startDictation} className="fa fa-2x fa-microphone icon"></i>
+    </div>
+
     <button type="submit" className="btn btn-outline-warning formButton">search</button>
   </form>
 <ul>
@@ -39,5 +51,67 @@ function App(){
     </div>);
 
 }
+
+//-------------------------------- speech recognition code---------------------------
+
+var final_transcript = '';
+var recognizing = false;
+
+  if ('webkitSpeechRecognition' in window) {
+    var recognition = new window.webkitSpeechRecognition();
+
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onstart = function () {
+      recognizing = true;
+      var audioElement = new Audio('images/speaknow.mp3');
+      audioElement.play();
+      document.getElementById("start_button").style.color = "red";
+    };
+
+    recognition.onerror = function (event) {
+      console.log(event.error);
+    };
+
+    recognition.onend = function () {
+      recognizing = false;
+      document.getElementById("start_button").style.color = "white";
+      var audioElement = new Audio('images/speaknow.mp3');
+      audioElement.play();
+    };
+
+    recognition.onresult = function (event) {
+      var interim_transcript = '';
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          final_transcript += event.results[i][0].transcript;
+        } else {
+          interim_transcript += event.results[i][0].transcript;
+        }
+      }
+      final_transcript = capitalize(final_transcript);
+      document.getElementById("formInput").value = interim_transcript;    
+      document.getElementById("formInput").value = final_transcript;  
+    };
+  }
+
+  function capitalize(s) {
+    return s.replace(s.substr(0, 1), function (m) {
+      return m.toUpperCase();
+    });
+  }
+
+  function startDictation() {
+    if (recognizing) {
+      recognition.stop();
+      return;
+    }
+    final_transcript = '';
+    recognition.lang = 'en-US';
+    recognition.start();
+    document.getElementById("formInput").innerHTML = '';
+    document.getElementById("formInput").innerHTML = '';
+  }
 
 export default App;
--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-function Item(props){
+var Item = (props)=>{
   var id = props.id;
   var href = "/"+id;
   var url = "https://books.google.com/books/content?id="+ id +"&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
+  var bookT = props.bookTitle;
+  bookT = bookT.substring(0, 50);
+  if((props.bookTitle).length >50) bookT = bookT+"...";
   return (<li className="listitems card books cardsss">
   <img className="card-img-top cardImg" src={url} alt={props.bookTitle}/>
   <div class="card-body">
-    <h5 class="card-title bookName">{props.bookTitle}</h5>
+    <h5 class="card-title bookName">{bookT}</h5>
     <h6>Ebook is {props.ebook ? <h6>available</h6>:<h6>unavailable</h6>}</h6>
     <a href={href} class="btn btn-primary">Details</a>
   </div>
   </li>);
-}
+};
 export default Item;
-<a href="/0" class="btn btn-primary">Enroll Now</a>
